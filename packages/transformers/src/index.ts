8c9804c3ad5e1c39ec8780308b143723a0f82042
xingxingmofash <xingxingmofashu@outlook.com>
2026-01-22 09:40:08 +0800

    chore: add transformers cli

--- packages/transformers/src/index.ts
+++ packages/transformers/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello from the transformers!");
+import yargs from 'yargs'
+import { hideBin } from "yargs/helpers"
+
+
+const cli = yargs(hideBin(process.argv))
+  .parserConfiguration({ "populate--": true })
+  .scriptName("transformers")
+  .wrap(100)
+  .help("help", "show help")
+  .alias("help", "h")
+
+try {
+  await cli.parse()
+} catch (error) {
+  process.exitCode = 1
+} finally {
+  // Some subprocesses don't react properly to SIGTERM and similar signals.
+  // Most notably, some docker-container-based MCP servers don't handle such signals unless
+  // run using `docker run --init`.
+  // Explicitly exit to avoid any hanging subprocesses.
+  process.exit()
+}
